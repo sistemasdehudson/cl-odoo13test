1aa85b6eb031ead4fdccefb1c03ec6bfc26045c1
concagabriel <58643247+concagabriel@users.noreply.github.com>
2022-06-13 20:12:48 -0300

    Update __manifest__.py

--- odoo13test_default/__manifest__.py
+++ odoo13test_default/__manifest__.py
@@ -72,7 +72,7 @@
         ##'https://github.com/ingadhoc/product -b 13.0',
         'https://github.com/OCA/wms.git -b 13.0',
         'https://github.com/filoquin/odoo_retail -b 13.0',
-        'https://github.com/OCA/queue/tree/13.0 -b 13.0',
+        'https://github.com/OCA/queue -b 13.0',
     ],
 
     'docker-images': [
